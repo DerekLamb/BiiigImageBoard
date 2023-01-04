8ce049ee01c86b5b7d7b482f897e9faa5be7b91c
Derek Lamb <dereklamb404@gmail.com>
2023-01-03 21:50:47 -0700

    Setup svelte commit
    Setting up svelte, mongodb, node server (Backend just test folder)

---
+++ frontend/src/components/imageBrowser.svelte
@@ -0,0 +1,29 @@
+
+<script>
+import Image from "./image.svelte";
+
+export let images = [{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1},{test:-1}];
+
+for (let i = 0; i > 100; i++){
+    images.push({test:i});
+}
+console.log(images)
+
+</script>
+
+<div class = "imageBrowser">
+    {#each images as image}
+        <Image ></Image>
+    {/each}
+</div>
+
+<style>
+    .imageBrowser{
+        display:flex;
+        flex-wrap: wrap;
+        min-width: 100px; 
+        min-height: 20px;
+
+        background-color: lightgreen
+    }
+</style>
