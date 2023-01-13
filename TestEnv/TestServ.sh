a22314faf38f11ade37a594115f19f1b7fa0c1d6
Derek Lamb <dereklamb404@gmail.com>
2023-01-13 00:46:49 -0700

    Dockerfile dev changes
    More Dockerfile dev progress

--- TestEnv/TestServ.sh
+++ TestEnv/TestServ.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mongosh <<EOF1
     use bib 
-    db.test_img.drop
+    db.dropDatabase()
     exit
 EOF1
 
@@ -10,7 +10,7 @@ files=($(find . -type f -iname "*.jpg" -or -iname "*.png"))
 for file in "${files[@]}"; do
 mongosh <<EOF2
     use bib
-    db.test_img.insertOne( {img_path: BinData(0,"$( cat $file | base64)" )} )
+    db.test_img.insertOne( {img_path: BinData(0,"$file" )} )
     exit
 EOF2
 done
