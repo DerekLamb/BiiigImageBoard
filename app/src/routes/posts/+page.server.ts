b2dc569409e4e29f5a0c66b9509f34253cfcbce5
Derek Lamb <dereklamb404@gmail.com>
2023-01-21 03:09:56 -0700

    Multifile upload + Display SSR
    Barebones Progress!!

---
+++ app/src/routes/posts/+page.server.ts
@@ -0,0 +1,20 @@
+import db from '$lib/db';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async () => {
+    const images = await db.collection('testimages').find().toArray();
+    
+    const rawImages = images.map(({ name, fsName, imagePath, tags}) => ({
+        name,
+        fsName,
+        imagePath,
+        tags
+    }))
+
+    console.log(rawImages)
+
+    return{
+        status: 200,
+        images: rawImages
+    }
+}
--- app/src/routes/posts/+page.svelte
+++ app/src/routes/posts/+page.svelte
@@ -1,19 +1,28 @@
 <script>
-    import ImageBrowser from "../../components/imageBrowser.svelte"
-    import Tagbar from "../../components/tagbar.svelte";
+    import Image from "$lib/image.svelte";
+    import Tagbar from "$lib/tagbar.svelte";
+    import { page } from '$app/stores';
+
+    /** @type {import('./$types').PageData} */ 
+    export let data;
+    let url = $page.url;
     
-    //set tag to specific size or move based on screen size
-    //decide image browser size 
 </script>
 
 <h1>b..d</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <div class = midContainer>
     <Tagbar ></Tagbar>
-    <ImageBrowser></ImageBrowser>
+    <div class = "imageBrowser">
+        {#each data.images as image}
+            <Image src = "../../{image.imagePath}" link = {$page.url}/{image.fsName} ></Image>
+        {/each}
+    </div>
 </div>
-
 <style>
+    :global(body){
+        background-color: rgb(10, 10, 10);
+    }
     .midContainer{
         display:flex;
     }
