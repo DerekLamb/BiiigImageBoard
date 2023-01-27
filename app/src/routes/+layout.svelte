f568863014d36140ba721ae36edac45667b25837
Derek Lamb <dereklamb404@gmail.com>
2023-01-27 13:41:26 -0700

    quick change

---
+++ app/src/routes/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+    import NavBar from "$lib/navBar.svelte";
+
+</script>
+
+<h1>b..d</h1>
+<p>Yeah... </p>
+<NavBar></NavBar>
+
+<slot></slot>
+
+<style>
+    h1{
+        color:azure;
+    }
+    p{
+        color:azure;
+    }
+</style>
