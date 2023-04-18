9c59b96e8fed86f35e7da98d98dc69ce2807c5b0
Derek L <dereklamb404@gmail.com>
2023-04-18 02:06:17 -0600

    Oh boy...
    changes: css layouts, got file processes working well, changed up some theming.

---
+++ app/src/routes/config/+page.server.ts
@@ -0,0 +1,27 @@
+import db from '$lib/db';
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ( url ) => {
+    const searchParams = url.url.searchParams
+    const currPage = ((searchParams.get("page") * 1) >= 1)?(searchParams.get("page")):1;
+    const pageLength = (searchParams.get("len"))?(searchParams.get("len") * 1):30;
+    const startInd = (currPage - 1) * pageLength;
+
+    const images = await db.collection('testimages').find().sort({ genName:1 }).skip(startInd).limit(pageLength).toArray();
+    const numPages = Math.ceil(await db.collection('testimages').countDocuments() / pageLength);
+    const rawImages = images.map(({ name, fsName, genName, imagePath, tags}) => ({
+        name,
+        fsName,
+        genName,
+        imagePath,
+        tags
+    }))
+
+
+    return{
+        status: 200,
+        images: rawImages,
+        pageNum: numPages,
+        currPage: currPage
+    }
+}
