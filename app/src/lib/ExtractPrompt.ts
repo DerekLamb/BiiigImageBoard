881005ea05e85c4376833c4f7e3317b81b2b5251
Derek Lamb <dereklamb404@gmail.com>
2023-04-19 16:34:28 -0600

    Small changes
    Changing processFiles and ExtractPrompt

--- app/src/lib/ExtractPrompt.ts
+++ app/src/lib/ExtractPrompt.ts
@@ -1,77 +1,48 @@
-
-
 export default function promptDecode(data:Buffer) {
-    let text = '';
+    let prompt = '';
     let name = '';
-    let param = false;
-    let debugLim = 2048;
+    let foundPrompt = false;
+    const limit = Math.min(data.length, 2048);
     let i = promptIndex(data)
+    const startIndex = promptIndex(data);
     console.log(i);
 
-    if (i){
-        while(!param){
-            let code = data[i]
-            console.log(code.toString(16))
-            if( code == 0 && !param){
-                console.log("found 00 char")
-                param = true;
-                break;
-            }
-            name += String.fromCharCode(code);
-            i++;
-        }
-        i++;
-        console.log(i);
-        while(param){
-            let code = data[i]
-            text += String.fromCharCode(code);
-            
-            if(code == 73 ){
-                console.log("found code == 73");
-                console.log(data[i+1])
-                if(data[i+1] == 68, data[i+2] == 65, data[i+3] == 84) {
+    if (startIndex){
+        for( let i = startIndex; i < limit; i +=1){
+            const currentByte = data[i];
 
-                    console.log("return condition")
-                    param = false;
-                    return {
-                        prop:name,
-                        prompt:text
-                    }
+            if(!foundPrompt){
+                //looking for end of 'name'
+                if(currentByte === 0){
+                    foundPrompt = true;
+                    i += 1; //skipping null char
+                }
+                else {
+                    name += String.fromCharCode(currentByte);
+                }
+            } else {
+                //looking for 'IDAT'
+                prompt += String.fromCharCode(currentByte);
+            
+                if(prompt.endsWith('IDAT')){
+                    return { prop: name, prompt }
                 }
             }
-            if(i >= debugLim){
-                param = false;
-                console.log("hit debuglim while searching for embedded text");
-                console.log(`prop:${name}`);
-                console.log(`propmt:${text}`);
-            }
-            i++;
         }
     }
-    else{
-        
-        console.log("no embedded text found")
-    }
-    
-    return {
-        prop: null,
-        prompt: null 
-    };
 }
 
 
 function promptIndex(dataBuff:Buffer){
     const checkRange = 48;
-    const checkBuff = dataBuff.slice(0,checkRange)
-
-    for(let j = 0; j < checkRange; j++){
-        if(checkBuff[j] == 116){
-            if(checkBuff[j+1] == 69 && checkBuff[j+2] == 88 && checkBuff[j+3] == 116 ){
-                console.log("found tEXT");
-                return j + 4;
-            }
+    const checkBuff = dataBuff.subarray(0,checkRange)
+    const uint32Arr = new Uint32Array(checkBuff.buffer);
+    
+    for (let j = 0; j < checkRange / Uint32Array.BYTES_PER_ELEMENT; j += 1) {
+        if (uint32Arr[j] === 0x54784574 /* 'tEXT' */) {
+          return j * Uint32Array.BYTES_PER_ELEMENT + 4;
         }
-    }
+      }
 
     return 0;
 }
