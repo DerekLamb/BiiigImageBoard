964c778eabf94bef2e4b12493a8e317e6b6f8aad
Derek Lamb <dereklamb404@gmail.com>
2023-05-18 14:37:45 -0600

    Image Waterfall Testing
    Starting a new image waterfall?

--- app/src/lib/childFileProc.ts
+++ app/src/lib/childFileProc.ts
@@ -1,12 +1,21 @@
-import { refreshFiles } from "./processFiles";
-
 const { spawn } = require('child_process');
 
 const child = spawn('node', ['procFiles.ts'], {
-    detached: true,
-    stdio: 'ignore'
+    detached: false
 });
 
 child.on('exit', function(code: any, signal: any){
     console.log('child process exited with code ${code} and signal ${signal}');
+});
+
+child.on('error', (err: Error) => {
+    console.error(`child process error: ${err}`);
+});
+
+child.stdout.on('data', (data: Buffer) => {
+    console.log(`stdout: ${data.toString()}`);
+});
+
+child.stderr.on('data', (data: Buffer) => {
+    console.error(`stderr: ${data.toString()}`);
 });
