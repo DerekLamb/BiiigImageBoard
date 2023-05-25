6483a08191cb37204d8b26c876826ca1e019d7bc
Derek L <dereklamb404@gmail.com>
2023-05-25 01:41:05 -0600

    More masonry/waterfall work
    Still testing and playing around with feature

--- app/src/lib/waterfallLayout.svelte
+++ app/src/lib/waterfallLayout.svelte
@@ -1,22 +1,41 @@
 <script>
     import Image from "./image.svelte";
+    import { onMount } from 'svelte';
     export let images = [];
     export let spacing = 20;
     export let imgWidth = 0;
 
-    function calculateLayout() {
-        const container = document.querySelector('.waterfall-container');
-        containerWidth = container.clientWidth;
+
+    let containerWidth;
+    let columns = [10, 10];
+    let columnWidth;
+    let gaps;
+    let container;
+
+    function calculateColumns() {
+        containerWidth = 200;
         const columnCount = Math.floor(containerWidth / 300);
         columns = new Array(columnCount).fill(0);
         columnWidth = (containerWidth - (columnCount + 1) * 5) / columnCount;
         gaps = new Array(columnCount).fill(5);
     }
+    
+    let promise = calculateColumns();
+
+     onMount(() => {
+         promise = calculateColumns();
+        window.addEventListener('resize', calculateColumns);
+     });
+
 </script>
 
-<div class="waterfallContainer" style="position:relative">
-    
-    <img src="{}" alt="Err">
+<div class="waterfallContainer" style="position:relative" bind:this={container}>
+    {#await calculateColumns()}
+    <p>...waiting</p>
+    {:then columns} 
+    <p>{columns}</p>
+    {/await}
+    <img src="" alt="Err">
 </div>
 
 <style>
--- app/src/routes/testing/+page.svelte
+++ app/src/routes/testing/+page.svelte
@@ -3,22 +3,46 @@
     import TagSection from "$lib/tagSection.svelte";
     import SideBar from "$lib/sideBar.svelte";
     import SearchBar from "$lib/searchBar.svelte";
-    import MasonryLayout from "$lib/waterfallLayout.svelte";
     import { onMount, beforeUpdate } from "svelte";
+	import { construct_svelte_component } from "svelte/internal";
     
     /** @type {import('./$types').PageData} */ 
     export let data;
 
+    let containerWidth;
+    let columns = [10, 10];
+    let columnWidth;
+    let gap = 5;
 
+    function calculateColumns() {
+        const columnCount = Math.floor(containerWidth / 300);
+        console.log(containerWidth);
+        // columns = new Array(columnCount).fill(0);
+        console.log("got here");
+        columnWidth = (containerWidth - (columnCount + 1) * 5) / columnCount;
+        
+        return {columns, columnWidth}
+    }
+    
+    let promise = calculateColumns();
 
-
-    console.log(...data.images);
+    // console.log(...data.images);
 
 </script>
-    <MasonryLayout images = {data.images} >
-
-    </MasonryLayout>
+    <div class="waterfallContainer" style="position:relative" bind:clientWidth={containerWidth}>
+        {#await promise}
+        <p>...waiting</p>
+        {:then grid} 
+        <p>{grid.columns}</p>
+        {/await}
+        <img src="" alt="Err">
+    </div>
 
 <style>
+    .waterfallContainer {
+        display: flex;
+        flex-wrap: wrap;
+        margin: 0 -5px;
+    }
 
 </style>
