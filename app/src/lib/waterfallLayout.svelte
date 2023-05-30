1316df952c29f929c2d6c3d493e09a1a451d46fb
Derek L <dereklamb404@gmail.com>
2023-05-30 02:15:09 -0600

    Waterfall layout
    refer to last commit

--- app/src/lib/waterfallLayout.svelte
+++ app/src/lib/waterfallLayout.svelte
@@ -1,30 +1,39 @@
 <script>
+	import { GridFSBucket } from "mongodb";
     import Image from "./image.svelte";
     import { onMount } from 'svelte';
+
+
     export let images = [];
     export let spacing = 20;
-    export let imgWidth = 0;
+    export let imgWidth = 0; // Unsure I actually need this but we will see
 
-
-    let containerWidth;
-    let columns;
-    let adjustRatios = [1.5, .75] //element aspect ratio snap points for fitting
+    const container = document.querySelector('.waterfallContainer');
+    const breakpointRange = [.75, 1.5] //images that fall in range set to 1:1 ratio, Faster way of checking?  
+    const columnBlockWidth = 300;
+    let grid
     let columnWidth;
+    let columns:[number];
     let gaps;
-    let container;
+
 
     function calculateColumns() {
-        containerWidth = 200;
-        const columnCount = Math.floor(containerWidth / 300);
-        columns = new Array(columnCount).fill(0);
-        columnWidth = (containerWidth - (columnCount + 1) * 5) / columnCount;
-        gaps = new Array(columnCount).fill(5);
+        let containerWidth = container.clientWidth;
+        let columnCount = Math.floor(containerWidth / columnBlockWidth);
+        let columns = new Array(columnCount).fill(0);
+        let columnWidth = (containerWidth - (columnCount + 1) * spacing) / columnCount;
+        let gaps = new Array(columnCount).fill(spacing);
+    }
+
+    function generateGrid() {
+        while(imgLeft){ //Don't like the name but can't beat it right now 
+            
+        }
     }
     
     let promise = calculateColumns();
 
      onMount(() => {
-         promise = calculateColumns();
         window.addEventListener('resize', calculateColumns);
      });
 
