ef21123e9eb9fe9c82ce346baf1022ae737faf30
Derek L <dereklamb404@gmail.com>
2023-11-17 02:32:46 -0700

    Class Organization progress

--- app/src/lib/fileService.ts
+++ app/src/lib/fileService.ts
@@ -1,34 +1,47 @@
 import fs from "fs/promises";
 import { constants } from 'fs';
 
-class imageFile extends File {
-    private directoryPath: string;
-    constructor(relativePath: string){
-        this.directoryPath = relativePath;
+class ImageFile extends File {
+    private dirPath: string;
+    private fileName: string;
+    private buffer: Buffer | null = null;
+
+    constructor(fileName: string, dirPathOrBuffer: string | Blob){
+        var dirPath = ""
+        var data: Blob;
+        if(dirPathOrBuffer instanceof Blob){
+            data = dirPathOrBuffer;
+        } else if(typeof dirPathOrBuffer === 'string') {
+            data = fs.readFile(`${this.dirPath}/${fileName}`)
+            dirPath = dirPathOrBuffer
+        }
+        super(data, fileName);
+        this.dirPath = dirPath;
+        this.fileName = fileName;
     }
 
     async create(fileName: string, data: Buffer, dataFormat:string = "base64"): Promise <void>{
-        fs.writeFile(`${this.directoryPath}/${fileName}`, data, "base64")
+        fs.writeFile(`${this.dirPath}/${fileName}`, data, "base64")
     }
 
     async read(fileName: string): Promise <Buffer | null>{
-        const fileData = await fs.readFile(`${this.directoryPath}/${fileName}`);
+        const fileData = await fs.readFile(`${this.dirPath}/${fileName}`);
         return fileData;
     }
 
-    async exists(fileName: string): Promise <boolean>{
+    async exists(): Promise<boolean> {
         try {
-            await fs.access(`${this.directoryPath}/${fileName}`, constants.F_OK);
-            return true;            
+            await fs.access(`${this.dirPath}/${this.fileName}`, constants.F_OK);
+            return true;
         } catch {
             return false;
         }
     }
 
     async delete(fileName: string): Promise <void>{
-            fs.unlink(`${this.directoryPath}/${fileName}`);
+            fs.unlink(`${this.dirPath}/${fileName}`);
     }
 }
 
 
-export default imageFileService
+export default ImageFile
