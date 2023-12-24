06a1b1140a258d9499a4339a33b54abf6f28acbf
Derek Lamb <dereklamb404@gmail.com>
2023-12-24 16:40:00 -0700

    Reorg and reimplement Thumbnail
    Reorganized and reimplemented the thumbnail generation and management

--- app/src/lib/fileUtilityService.ts
+++ app/src/lib/fileUtilityService.ts
@@ -1,25 +1,71 @@
-import type imageFileService from "$lib/fileService";
+import type {FileRepository} from "$lib/fileService";
+import {mainFileRepo} from "$lib/fileService";
+import type { ImageRepository } from "./imageRepository";
+
+import sharp from 'sharp';
 import crypto from 'crypto';
+import promptDecode from "$lib/ExtractPrompt";
 
 class fileUtiltyService {
     private fileServiceObj;
-    constructor(fileServiceObj: imageFileService){
+
+    constructor(fileServiceObj: FileRepository){
         this.fileServiceObj = fileServiceObj;
     }
 
-    //async extractPrompt(){}
+    async createThumbnail(fileName: string, file?: Buffer):Promise<[string, Buffer | null]>{ //returns a tuple of [string, buffer]
+        if(!file){
+            try{
+                file = await this.fileServiceObj.readFile(fileName);
+            } catch (error) {
+                throw new Error(`Error reading file ${fileName}: ${error}`)
+            }
+        }
+        const thumbName = `${fileName}_thmb.webp`;
+        let thumbData = null;
+        try {
+            thumbData = await sharp(file)
+            .resize({width: 200})
+            .webp()
+            .toBuffer();
+
+        } catch (error) {
+            console.log("err occured writing file")
+        }
+        return [thumbName, thumbData]
+    }
+
+    async extractPrompt(file: string | Buffer){
+        let fileData = null;
+        if(typeof file === 'string'){
+            fileData = await this.fileServiceObj.readFile(file);
+        }
+        else if (file instanceof Buffer){
+            fileData = file;
+        }
+        else{
+            throw new Error('Invalid input. Expecting string or Buffer.');
+        }
+        
+        if(fileData){
+            const prompt = promptDecode(fileData);
+            if(prompt){
+                return prompt;
+            }
+        }
+    }
 
     async hashFile(file: string | Buffer): Promise<string> {
         let buffer: Buffer;
 
         if (typeof file === 'string') {
-            buffer = fs.readFileSync(file);
+            buffer = await this.fileServiceObj.readFile(file);
         } else if (typeof file === 'object' && file instanceof Buffer) {
             buffer = file;
         } else {
             throw new Error('Invalid input. Expecting string or Buffer.');
         }
-    
+
         try {
             const hash = crypto.createHash('sha256');
             hash.update(buffer);
@@ -28,6 +74,39 @@ class fileUtiltyService {
         } catch (error: any) {
             throw new Error('Error while hashing file: ' + error.message);
         }
+
+    } 
+
+    async checkAllFiles(imageRepository: ImageRepository){
+        const filelist = await this.fileServiceObj.updateFiles()
+        const dblist = await imageRepository.get();
+        
+        const dbFileNames = dblist.map((document) => document.sanitizedFilename);
+        const dirFileNames = Array.from(filelist.keys());
+
+        const missingFiles = dbFileNames.filter( file => !dirFileNames.includes(file));
+        const missingDBRecord = dirFileNames.filter( file => !dbFileNames.includes(file));
+        return [missingFiles, missingDBRecord]
     }
 
+    async missingThumbAll(imageRepository: ImageRepository, thumbFileRepo: FileRepository){
+        //const filelist = await this.fileServiceObj.updateFiles();
+        const results = await imageRepository.get({ $or: [{thumbnailPath: {$exists:null}}, {thumbnailPath: ""}]});
+        console.log(results);
+        
+        Promise.all(results.map( async (image) => {
+            const thumb = await this.createThumbnail(
+                image.sanitizedFilename,
+                await this.fileServiceObj.readFile(image.sanitizedFilename)
+            );
+
+            if(thumb[1] && image._id) {
+                thumbFileRepo.addFile(thumb[0], thumb[1]);
+                imageRepository.update(image._id.toString(), {thumbnailPath: `thumb/${thumb[0]}`});
+            }
+        }))
+    }
 }
+
+export const fileUtilService = new fileUtiltyService(mainFileRepo);
+export type {fileUtiltyService};
--- app/src/routes/upload/+page.server.ts
+++ app/src/routes/upload/+page.server.ts
@@ -1,64 +1,68 @@
-import {mainFileRepo} from "$lib/fileService"
+import {mainFileRepo, thumbFileRepo} from "$lib/fileService"
 import {imageRepo} from "$lib/imageRepository"
 import { hashFile } from "$lib/processFiles.js";
+import { fileUtilService } from "$lib/fileUtilityService.js";
 
 
-    async function checkFiles(){
-        const filelist = await mainFileRepo.updateFiles()
-        const dblist = await imageRepo.getAll();
-        const dbFileNames = dblist.map((document) => document.sanitizedFilename);
-        const dirFileNames = Array.from(filelist.keys());
+async function checkFiles(){
+    const filelist = await mainFileRepo.updateFiles()
+    const dblist = await imageRepo.get();
+    const dbFileNames = dblist.map((document) => document.sanitizedFilename);
+    const dirFileNames = Array.from(filelist.keys());
 
-        const missingFiles = dbFileNames.filter( file => !dirFileNames.includes(file));
-        const missingDB = dirFileNames.filter( file => !dbFileNames.includes(file));
-        await Promise.all(missingDB.map( async file => {
-            imageRepo.create(
-                file,
-                file,
-                file.split('.')[0],
-                "images",
-                "",
-                await hashFile(`images/${file}`),
-            )
-            console.log(file)
-        }))
-    }
+    const missingFiles = dbFileNames.filter( file => !dirFileNames.includes(file));
+    const missingDB = dirFileNames.filter( file => !dbFileNames.includes(file));
+    await Promise.all(missingDB.map( async file => {
+        imageRepo.create(
+            file,
+            file,
+            file.split('.')[0],
+            "images",
+            "",
+            await hashFile(`images/${file}`),
+        )
+        console.log(file)
+    }))
+}
 
-    /** @type {import('./$types').Actions} */
+/** @type {import('./$types').Actions} */
 
 
-    export const actions = {
+export const actions = {
 
-        default: async ({ request }) => {
-            const formdata = await request.formData(); 
-            const files = formdata.getAll("image");
-            await Promise.all(files.map( async file => {
-                if(!(file instanceof Object) || !file.name){
-                    console.log("Error processing file attributes/data")
-                } else {
-                    
-                    try{
-                        const buffer =  Buffer.from(await file.arrayBuffer());
-                        const hash = await hashFile(buffer);
-                        const [ base, ext ] = file.name.split('.');
-                        const timestamp = base.match(/\b\d{13}\b/) ? base : `${Date.now().toString()}`;
-                        const newFileName = `${timestamp}.${ext}`
-
-                        
-                        const dbResults = await imageRepo.create(file.name, newFileName, timestamp, "images", "", hash)
-                        if(dbResults){
-                            await mainFileRepo.addFile(newFileName, buffer);
-                        }
-                    } catch (error) {
-                        console.log(error);
+    default: async ({ request }) => {
+        const formdata = await request.formData(); 
+        const files = formdata.getAll("image");
+        await Promise.all(files.map( async file => {
+            if(!(file instanceof Object) || !file.name){
+                console.log("Error processing file attributes/data")
+            } else {
+                
+                try{
+                    const buffer =  Buffer.from(await file.arrayBuffer());
+                    const hash = await fileUtilService.hashFile(buffer);
+                    const [ base, ext ] = file.name.split('.');
+                    const timestamp = base.match(/\b\d{13}\b/) ? base : `${Date.now().toString()}`;
+                    const newFileName = `${timestamp}.${ext}`
+                    const dbResults = await imageRepo.create(file.name, newFileName, timestamp, "images", "", hash)
+                    if(dbResults){
+                        await mainFileRepo.addFile(newFileName, buffer);
                     }
-                    
+                } catch (error) {
+                    console.log(error);
                 }
-            }))
+                
+            }
+        }))
 
-            //checkFiles();
-            return { sucess: true, submitted: files.length };
+        try{
+            console.log("Checking for missing files")
+            await fileUtilService.missingThumbAll(imageRepo, thumbFileRepo);
+        } catch (error) {
+            console.log(error);
         }
-    };
+        return { sucess: true, submitted: files.length };
+    }
+};
 
 
