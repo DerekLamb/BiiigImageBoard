98ffcf56b9a2a702006cf07d02e2f316284dc85d
Derek L <dereklamb404@gmail.com>
2024-04-10 17:52:11 -0600

    Docker compose work

--- app/src/lib/db.ts
+++ app/src/lib/db.ts
@@ -1,10 +1,13 @@
-import { MongoClient } from "mongodb"
+import { MongoClient } from "mongodb";
 import { env } from "$env/dynamic/private";
 
-const DB_URI = env.DB_URI || "mongodb://localhost:27017"
+const DB_URI = env.DB_URI || "mongodb://localhost:27017";
 const dbName = 'bib';
-const client = new MongoClient(DB_URI)
+const client = new MongoClient(DB_URI);
 
-await client.connect()
+export function start_mongo() {
+    console.log("Connecting to MongoDB");
+    return client.connect();
+}
 
-export const db = client.db(dbName)
+export const db = client.db(dbName);
