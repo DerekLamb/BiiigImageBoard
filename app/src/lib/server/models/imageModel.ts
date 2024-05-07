9d6fa59c67dcb969665689fd44c27911acdba0be
Derek L <dereklamb404@gmail.com>
2024-05-07 17:34:14 -0600

    Moving blocks around

--- app/src/lib/server/models/imageModel.ts
+++ app/src/lib/server/models/imageModel.ts
@@ -1,22 +1,6 @@
 import { ObjectId, type Sort, type Collection } from "mongodb";
 import { collections, db } from "$lib/db";
-
-
-const imageCollection : Collection<ImageDoc> = db.collection(collections.images);
-
-interface BaseImage{ // This is the interface for the image data that is stored in the database
-    originalName: string;
-    sanitizedFilename: string;
-    imagePath: string;
-    uploadDate: string;
-    thumbnailPath: string;
-    groups: string[];
-    tags: string[];
-    embPrompt?: string[][]; //needs fleshed out
-    related?: string[]; 
-    favorite?: string[];
-    hidden?: string[];
-}
+import { type BaseImage, imageCollection } from "./unifiedModel";
 
 interface ImageDoc extends BaseImage { // 
     _id: ObjectId;
@@ -25,7 +9,6 @@ interface ImageDoc extends BaseImage { //
 export interface AppImageData extends BaseImage {
     _id: string,
 }
-
  
 
 function toClient(document: ImageDoc): AppImageData {
