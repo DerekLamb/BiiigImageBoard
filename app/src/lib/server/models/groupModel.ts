f5e80afd66acb61e41ab20027b8c6463325a80dc
Derek L <dereklamb404@gmail.com>
2024-05-08 22:36:17 -0600

    Feature Work

--- app/src/lib/server/models/groupModel.ts
+++ app/src/lib/server/models/groupModel.ts
@@ -1,5 +1,6 @@
 import { ObjectId, type Sort } from 'mongodb';
 import { ImageModel } from './imageModel';
+import { collections, db } from '$lib/db';
 import { imageCollection, groupCollection, type BaseImage, type BasicGroup} from './unifiedModel';
 
 interface GroupDoc extends BasicGroup {
@@ -36,12 +37,27 @@ export const GroupModel = {
         return toClient(document);
     },
 
-    async getGroupChildren(id: string) {
-        const document = await groupCollection.findOne({ _id: new ObjectId(id) }) as GroupDoc;
-        return document.children; // returns array of imageIds or groupIds
+    async getGroupChildren(id: string, page = 0, limit = 10) {
+        const topLevel = imageCollection.aggregate([
+            {
+                $unionWith: {
+                    coll: collections.groups,
+                    pipeline: [ { $match: { groups: [] } }]
+                }
+            },
+            { $match: { groups: [] } },  // Only include top-level folders
+            { $sort: { uploadDate: -1  } },  // Sort by name or other criteria
+            { $skip: 0 },  // Pagination offset, calculate based on current page
+            { $limit: 10 }  // Number of items per page
+        ])
+
+        for await ( const doc of topLevel ) {
+            console.log(doc);
+        }
+
     },
 
-    async createGroup(groupData: AppGroupData) {
+    async createGroup(groupData: Partial<AppGroupData>) {
         return await groupCollection.insertOne(toDatabase(groupData)); 
     },
 
--- app/src/routes/api/groups/+server.ts
+++ app/src/routes/api/groups/+server.ts
@@ -6,6 +6,15 @@ export async function POST({ request } : Request){
     try {
         const body = await request.json();
         console.log(body);
+
+        GroupModel.createGroup({name: new Date().toISOString(), 
+            uploadDate: new Date().toISOString(), 
+            children: [body.draggedImage, body.draggedOverImage], 
+            groups: [], 
+            groupType: 'default', 
+            groupTags: []});
+
+        GroupModel.getGroupChildren(body.id, body.page, body.limit);
         // Insert image into group collection if first element is image 
 
         return json({ success: true });
