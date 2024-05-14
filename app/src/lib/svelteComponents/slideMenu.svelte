eb261ed0859d967a4c46fe69d2a395686cead612
Derek L <dereklamb404@gmail.com>
2024-05-14 09:54:58 -0600

    Adding Components

---
+++ app/src/lib/svelteComponents/slideMenu.svelte
@@ -0,0 +1,18 @@
+<script >
+    export 
+
+</script>
+
+<!-- Off-Canvas Menu for Mobile -->
+<div class="menu-container" class:open={$menuOpen} on:click={toggleMenu} on:keydown={toggleMenu} role="menu" tabindex=0>
+    <!-- Mobile Menu Content -->
+    <nav>
+        <ul>
+            <li><a href="/posts">Posts</a></li>
+            <li><a href="/tags">Tags</a></li>
+            <li><a href="/upload">Upload</a></li>
+            <li><a href="/">Home</a></li>
+            <li><a href="/login">Login</a></li>
+        </ul>
+    </nav>
+</div>  
