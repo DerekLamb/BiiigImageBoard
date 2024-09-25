57aecea9cc71bcb65e7eceaf488370bf3b690c2b
Derek L <dereklamb404@gmail.com>
2024-09-25 00:06:35 -0600

    Structure changes

--- app/src/lib/server/services/imageService.ts
+++ app/src/lib/server/services/imageService.ts
@@ -1,7 +1,7 @@
 import { ImageModel } from "../models/imageModel"
 import { type AppImageData } from "../models/imageModel"
 import { FileModel } from "../models/fileModel"
-import { imageUtil } from "../utility/imageUtil"
+import { imageFileUtil } from "../utility/imageUtil"
 
 
 export const imageService = {
@@ -18,10 +18,11 @@ export const imageService = {
         }
     
         buffer = buffer ? buffer : await FileModel.read(image.imagePath);
-        const thumbnail = await imageUtil.createThumbnail(buffer);
+        const thumbnail = await imageFileUtil.createImageThumbnail(buffer);
         const thumbnailname = `${image.sanitizedFilename}_thmb.webp`;
         const thumbnailPath = `thumb/${thumbnailname}`;
         try{
+            
             await ImageModel.updateImage(image._id, "thumbnailPath", thumbnailPath); // I know I know, reusing thumbnailPath twice. I want to replace with a constant from a central lib file TODO 
 
             try{
