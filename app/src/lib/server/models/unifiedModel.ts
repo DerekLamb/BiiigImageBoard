48ba1df655e5bcfca948153c33933bdafb6774ba
Derek L <dereklamb404@gmail.com>
2025-03-03 17:46:52 -0700

    Finally cleaning out some old code

--- app/src/lib/server/models/unifiedModel.ts
+++ app/src/lib/server/models/unifiedModel.ts
@@ -5,55 +5,13 @@ import { databaseDocUtil  as dbUtil } from "$lib/server/utility/dbUtil";
 import { GroupModel } from "$lib/server/models/groupModel";
 
 
-// export interface BaseImage { // This is the interface for the image/group data that is stored in the database
-//     originalName: string;
-//     sanitizedFilename: string;
-//     imagePath: string;
-//     uploadDate: string;
-//     thumbnailPath: string; //thumbnail exists as path to file
-//     groups: string[];
-//     tags: string[];
-//     embPrompt?: string[][]; //needs 2b fleshed out
-//     related?: string[]; 
-//     favorite?: string[];
-//     hidden?: string[];
-// }
-
-// export interface BasicGroup {
-//     name: string, // name of group
-//     uploadDate: string, // date group was created
-//     children: string[], // contains the ids of imageDoc or other GroupDoc(s), will need to handle making sure only goes three levels deep for groups
-//     groups: string[], // needs considerations 
-//     groupType: string, // possible extension, unsure what to use for now
-//     groupTags: string[], // tags for the group 
-// }
-
-export interface BaseDoc {
-    name: string;
-    filePath: string;
-    createDate: string;
-    thumbnailPath: string;
-    groups: string[];
-    tags: string[];
-    type: "image" | "group";
-}
-
-interface GroupDoc extends BasicGroup {
-    _id: ObjectId;
-}
-
-interface ImageDoc extends BaseImage { // 
-    _id: ObjectId;
-}
-
-
 export const UnifiedModel = {
     
     async getDocumentById(id: string){
-        let document = false; //todo
+        let document = false; //TODO
     },
 
-    async findNodeChildren(groupName: string = "", limit = 10, skip = 0, sort = { uploadDate: -1}) { //used to get children of a group/node
+    async findNodeChildren(groupName: string = "", limit = 10, skip = 0, sort = { uploadDate: -1}) {
         let processedDocuments
 
         if(groupName == "" || groupName == null) {
@@ -83,8 +41,8 @@ export const UnifiedModel = {
             },
             matchStage,  
             { $sort: sort },
-            { $skip: skip },  // Pagination offset, calculate based on current page
-            { $limit: limit }  // Number of items per page
+            { $skip: skip },  
+            { $limit: limit }
         ])
         let aggArr = await topLevel.toArray()
         const processedDocuments = aggArr.map(dbUtil.convertIdToString);
