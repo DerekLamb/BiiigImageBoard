6344a75d5e7e2b6288d6d56c10256bb2f1d4c85f
Derek L <dereklamb404@gmail.com>
2025-03-05 21:46:08 -0700

    Adding Testing!
    Yay!

--- app/vite.config.js
+++ app/vite.config.js
@@ -22,6 +22,15 @@ const config = {
 		},
 		proxy:{}, //unsure why but needed for dev https
 	},
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		coverage: {
+			reporter: ['text', 'json', 'html'],
+			include: ['src/lib/server/models/**/*.ts']
+		}
+	},
 };
 
 
