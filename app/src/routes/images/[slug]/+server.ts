567e5ebaa1e015012ee9a4b7b9b3e56fdf0eda95
Derek L <dereklamb404@gmail.com>
2025-03-09 19:12:27 -0600

    Create +server.ts

---
+++ app/src/routes/images/[slug]/+server.ts
@@ -0,0 +1,32 @@
+
+import { fileController } from "$lib/server/controllers/fileController.js";
+import { error } from  "@sveltejs/kit"
+import path from "path";
+
+/** @type {import('./$types').RequestHandler} */
+
+export async function GET ({ params, locals}){
+    if(!locals.user){
+        error(401, "Unauthorized");
+    }
+
+    if(!params.slug){ // need to build a better file type system TODO
+        error(400, "invalid image request");
+    }
+
+    try{
+        console.log(params.slug);
+        const imageBuffer = await fileController.getFile(params.slug); // will need to rip out eventually 
+        const imageType = path.extname(params.slug).substring(1); // need to build a better file type system
+        console.log("running this code");
+        return new Response(imageBuffer, {
+            headers: {
+                'Content-Type': `image/${imageType}`,
+                'Cache-Control': 'max-age=31536000, immutable'
+            }
+        })
+    }
+    catch(e){
+        return new Response("Image not found", {status: 404});
+    }
+}
